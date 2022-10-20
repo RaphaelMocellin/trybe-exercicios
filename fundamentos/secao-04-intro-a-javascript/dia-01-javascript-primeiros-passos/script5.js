1f0488f22298a984232ba7d5286a70237cad97a9
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-20 14:55:38 -0300

    exercicios com ||

---
+++ fundamentos/secao-04-intro-a-javascript/dia-01-javascript-primeiros-passos/script5.js
@@ -0,0 +1,16 @@
+let weekDay = 'sábadlolo';
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+
+    console.log('Oba, mais um dia de aprendizado na Trybe!')
+
+} else if (weekDay === 'sábado' || weekDay === 'domingo') {
+
+    console.log('FINALMENTE, descanso merecido!')
+
+} 
+else {
+
+    console.log('Não sei que dia é este!')
+
+}
