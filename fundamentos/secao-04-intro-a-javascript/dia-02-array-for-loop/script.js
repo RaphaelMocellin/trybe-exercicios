f2bbf9edd9fcc493cd6da8defcf7cf735f6c34d3
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-21 15:05:52 -0300

    finalizados exercícios do 1 ao 5

---
+++ fundamentos/secao-04-intro-a-javascript/dia-02-array-for-loop/script.js
@@ -0,0 +1,56 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// // Exercício 1 - Imprima todos os valores
+// for (let number of numbers) {
+//     console.log(number);
+// }
+
+// Exercício 2 - some todos os valores e imprima o resultado
+
+// let soma = 0;
+// for(let number of numbers) {
+//     soma = soma + number;
+// }
+// console.log(soma);
+
+// Exercício 3 - Média aritimética
+
+// let soma = 0;
+// let media;
+
+// for(let number of numbers) {
+//     soma = soma + number;
+// }
+
+// media = soma / numbers.length;
+
+// console.log(media);
+
+// Exercício 4 - Imprimir maior que 20 ou menor que 20
+
+// let soma = 0;
+// let media;
+
+// for (let number of numbers) {
+//   soma = soma + number;
+// }
+
+// media = soma / numbers.length;
+
+// if (media > 20) {
+//   console.log("Valor maior que 20!");
+// } else {
+//   console.log("Valor menor ou igual a 20!");
+// }
+
+// Exercício 5 - Qual maior valor do array
+
+let maiorValor = 0;
+
+for (number of numbers) {
+    if (number > maiorValor) {
+        maiorValor = number;
+    }
+}
+
+console.log(maiorValor);
