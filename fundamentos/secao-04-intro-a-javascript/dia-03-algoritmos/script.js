4cab58135449a7f643e28aeca918d3cc2f03edbd
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-24 15:12:09 -0300

    Finalizados exercícios 1 e 2

---
+++ fundamentos/secao-04-intro-a-javascript/dia-03-algoritmos/script.js
@@ -0,0 +1,20 @@
+// Exercício 1 - Fatorial de 10!
+
+// let factorial = 1;
+
+// for (let i = 10; i >= 1; i -= 1) {
+//     factorial *= i;
+// }
+
+// console.log(factorial);
+
+// Exercício 2 - Inverter palavra
+
+// let word = "tryber";
+// let invertedWord = "";
+
+// for (let i = word.length - 1; i >= 0; i-= 1) {
+//     invertedWord += word[i];
+// }
+
+// console.log(invertedWord);
