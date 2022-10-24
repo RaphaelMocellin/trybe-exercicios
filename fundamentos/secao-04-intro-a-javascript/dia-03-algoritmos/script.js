76ec4325b97356217d29d7c9c855d4f8355a2b30
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-24 15:23:30 -0300

    finalizado exercício 3

--- fundamentos/secao-04-intro-a-javascript/dia-03-algoritmos/script.js
+++ fundamentos/secao-04-intro-a-javascript/dia-03-algoritmos/script.js
@@ -18,3 +18,25 @@
 // }
 
 // console.log(invertedWord);
+
+// Exercício 3 - Maior e menor do Array
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let bigger = array[0].length;
+// let smaller = array[0].length;
+
+
+// for (let i = 1; i < array.length; i += 1) {
+//     if (array[i].length > array[i - 1].length) {
+//         bigger = array[i];
+//     }
+// }
+
+// for (let i = 1; i < array.length; i += 1) {
+//     if (array[i].length < array[i - 1].length) {
+//         smaller = array[i];
+//     }
+// }
+
+// console.log("A maior palavra é: " + bigger);
+// console.log("A menor palavra é: " + smaller);
