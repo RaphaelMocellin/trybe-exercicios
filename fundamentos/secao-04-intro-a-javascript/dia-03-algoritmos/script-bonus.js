d24bb6b817bb708a73b0dde9ec2791a3be3b379a
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-24 16:10:44 -0300

    finalizado exercício bonus 1

---
+++ fundamentos/secao-04-intro-a-javascript/dia-03-algoritmos/script-bonus.js
@@ -0,0 +1,12 @@
+// Exercício bonus 1
+
+// let n = 5;
+// let asteriscos = '';
+
+// for (let i = 1; i <= n; i += 1) {
+//     asteriscos = asteriscos + '*';
+// }
+
+// for (let i = 1; i <=n; i += 1) {
+//     console.log(asteriscos);
+// }
