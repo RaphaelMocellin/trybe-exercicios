dcdb3d5abf6d4ff3bb8c98412fce7da91d58cc9c
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-25 16:38:11 -0300

    finalizado exercício 1

---
+++ fundamentos/secao-04-intro-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -0,0 +1,9 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+// Exercício 1 - Imprima as boas vindas
+
+console.log('Bem-vinda, ' + info.personagem);
