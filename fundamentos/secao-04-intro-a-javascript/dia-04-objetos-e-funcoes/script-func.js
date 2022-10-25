807e8fc7d9d3716ddac859c7dda726a300c3c2c4
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-25 18:07:50 -0300

    finalizado o exercício 3 de funções

--- fundamentos/secao-04-intro-a-javascript/dia-04-objetos-e-funcoes/script-func.js
+++ fundamentos/secao-04-intro-a-javascript/dia-04-objetos-e-funcoes/script-func.js
@@ -27,18 +27,36 @@
 
 // Exercício 2 - Crie uma função que receba um array de inteiros e retorno o index do maior valor.
 
-let arrayTeste = [2, 3, 6, 7, 10, 1];
+// let arrayTeste = [2, 3, 6, 7, 10, 1];
 
-function biggestInArray(array) {
-    let biggest;
+// function biggestInArray(array) {
+//     let biggest;
+//     for (let i = 0; i < array.length; i += 1) {
+//         for (let i2 = 0; i2 < array.length; i2 += 1) {
+//             if (array[i] > array[i2]){
+//                 biggest = array[i];
+//             }
+//         }
+//     }
+//     return console.log(array.indexOf(biggest));
+// }
+
+// biggestInArray(arrayTeste);
+
+// Exercício 3 - Crie uma função que receba um array de inteiros e retorno o index do menor valor.
+
+let arrayTeste = [2, 3, 6, 7, 10, 0, -3];
+
+function smallestInArray(array) {
+    let smallest;
     for (let i = 0; i < array.length; i += 1) {
         for (let i2 = 0; i2 < array.length; i2 += 1) {
-            if (array[i] > array[i2]){
-                biggest = array[i];
+            if (array[i] < array[i2]){
+                smallest = array[i];
             }
         }
     }
-    return console.log(array.indexOf(biggest));
+    return console.log(array.indexOf(smallest));
 }
 
-biggestInArray(arrayTeste);
+smallestInArray(arrayTeste);
