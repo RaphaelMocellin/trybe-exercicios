5640343cf515d66540aef8c8081fe792601f1c1b
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-26 15:15:58 -0300

    finalizado exercício 2 da parte 2

--- fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
+++ fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
@@ -2,19 +2,33 @@
 
 // Exercício 1 - Crie uma função que ligue e desligue um motor de um carro.
 
-let statusMotor = "ligado";
+// let statusMotor = "ligado";
 
-const ligarDesligarMotor = () => (statusMotor === "ligado" ? statusMotor = "Desligado" : statusMotor = "ligado");
+// const ligarDesligarMotor = () => (statusMotor === "ligado" ? statusMotor = "Desligado" : statusMotor = "ligado");
 
 
-ligarDesligarMotor();
-console.log(`O motor está ${statusMotor}`);
+// ligarDesligarMotor();
+// console.log(`O motor está ${statusMotor}`);
 
-ligarDesligarMotor();
-console.log(`O motor está ${statusMotor}`);
+// ligarDesligarMotor();
+// console.log(`O motor está ${statusMotor}`);
 
-ligarDesligarMotor();
-console.log(`O motor está ${statusMotor}`);
+// ligarDesligarMotor();
+// console.log(`O motor está ${statusMotor}`);
 
-ligarDesligarMotor();
-console.log(`O motor está ${statusMotor}`);
+// ligarDesligarMotor();
+// console.log(`O motor está ${statusMotor}`);
+
+// Exercício 2 - Crie uma função que calcule a área de um círculo.
+
+const circleArea = (raio) => {
+    let area = 0;
+    const pi = 3.14;
+
+    area = pi * raio ** 2;
+
+    return console.log(`Essa é a área do círculo: ${area}`);
+}
+
+circleArea(3);
+circleArea(5);
