1d5ef6db5c44c6f47bca38a947078184b908b003
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-26 15:28:53 -0300

    finalizado exercício 3 da parte 2

--- fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
+++ fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
@@ -21,14 +21,40 @@
 
 // Exercício 2 - Crie uma função que calcule a área de um círculo.
 
-const circleArea = (raio) => {
-    let area = 0;
-    const pi = 3.14;
+// const circleArea = (raio) => {
+//     let area = 0;
+//     const pi = 3.14;
 
-    area = pi * raio ** 2;
+//     area = pi * raio ** 2;
 
-    return console.log(`Essa é a área do círculo: ${area}`);
+//     return console.log(`Essa é a área do círculo: ${area}`);
+// }
+
+// circleArea(3);
+// circleArea(5);
+
+// Exercício 3 - Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+const longestWord = (phrase) => {
+    let splitedPhrase = phrase.split(" ");
+    let longestCount = 0;
+    let longestWord;
+
+    for (let word of splitedPhrase) {
+        for (let word2 of splitedPhrase) {
+            if (word.length > word2.length && word.length > longestCount) {
+                longestCount = word.length;
+                longestWord = word;
+            }
+        }
+    }
+
+    console.log(longestWord);
 }
 
-circleArea(3);
-circleArea(5);
+// Exercício 3 - utilizando método array.sort().
+
+
+longestWord('apenas um teste');
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
+longestWord('apenas um teste para saber qual a maior palavra desta maldita frase');
