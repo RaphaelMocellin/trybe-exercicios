24b817e94ede34ee31ab343c8bd2f667ae26bb53
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-26 15:49:15 -0300

    finalizado exercício 3 com bonus de elaborado com array.sort()

--- fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
+++ fundamentos/secao-04-intro-a-javascript/dia-05-es6/script-part2.js
@@ -35,24 +35,31 @@
 
 // Exercício 3 - Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
 
-const longestWord = (phrase) => {
-    let splitedPhrase = phrase.split(" ");
-    let longestCount = 0;
-    let longestWord;
+// const longestWord = (phrase) => {
+//     let splitedPhrase = phrase.split(" ");
+//     let longestCount = 0;
+//     let longestWord;
 
-    for (let word of splitedPhrase) {
-        for (let word2 of splitedPhrase) {
-            if (word.length > word2.length && word.length > longestCount) {
-                longestCount = word.length;
-                longestWord = word;
-            }
-        }
-    }
+//     for (let word of splitedPhrase) {
+//         for (let word2 of splitedPhrase) {
+//             if (word.length > word2.length && word.length > longestCount) {
+//                 longestCount = word.length;
+//                 longestWord = word;
+//             }
+//         }
+//     }
 
-    console.log(longestWord);
-}
+//     console.log(longestWord);
+// }
 
 // Exercício 3 - utilizando método array.sort().
+
+const longestWord = (phrase) => {
+    let splitedPhrase = phrase.split(" ");
+    let sortedPhrase = splitedPhrase.sort(function(a, b){ return b.length - a.length});
+
+    console.log(sortedPhrase[0]);
+}
 
 
 longestWord('apenas um teste');
