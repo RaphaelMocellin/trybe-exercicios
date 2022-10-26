7107bf5716179aef8b761bda53210c19a3951605
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-26 11:48:06 -0300

    finalizado exercicio 4 bonus, assim como todos que deu tempo

--- fundamentos/secao-04-intro-a-javascript/dia-04-objetos-e-funcoes/bonus.js
+++ fundamentos/secao-04-intro-a-javascript/dia-04-objetos-e-funcoes/bonus.js
@@ -70,3 +70,40 @@
     // for (let fruit in basketObj) {
     //     console.log(basketObj[fruit], fruit);
     // }
+
+    // Exercício 4 - Faça o pedido com o obj
+
+    let moradores = {
+        blocoUm: [
+          {
+            nome: 'Luiza',
+            sobrenome: 'Guimarães',
+            andar: 10,
+            apartamento: 1005,
+          },
+          {
+            nome: 'William',
+            sobrenome: 'Albuquerque',
+            andar: 5,
+            apartamento: 502,
+          },
+        ],
+        blocoDois: [
+          {
+            nome: 'Murilo',
+            sobrenome: 'Ferraz',
+            andar: 8,
+            apartamento: 804,
+          },
+          {
+            nome: 'Zoey',
+            sobrenome: 'Brooks',
+            andar: 1,
+            apartamento: 101,
+          },
+        ],
+      };
+
+    //   console.log("O morador do bloco 2 de nome " + moradores.blocoDois[moradores.blocoDois.length - 1].nome + " " + moradores.blocoDois[moradores.blocoDois.length - 1].sobrenome + " mora no " + moradores.blocoDois[moradores.blocoDois.length - 1].andar + "º andar, apartamento " + moradores.blocoDois[moradores.blocoDois.length - 1].apartamento + ".");
+
+    
