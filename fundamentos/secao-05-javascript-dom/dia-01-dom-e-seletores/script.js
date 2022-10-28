9f61ccc0df2623733f6f47b9eb26ce40848d126f
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-10-28 15:29:40 -0300

    finalizado exercicios de antes da aula ao vivo

---
+++ fundamentos/secao-05-javascript-dom/dia-01-dom-e-seletores/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#header-container').style.backgroundColor =  'green';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'orange';
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = 'purple';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = 'black';
+document.querySelector('#footer-container').style.backgroundColor =  'darkgreen';
