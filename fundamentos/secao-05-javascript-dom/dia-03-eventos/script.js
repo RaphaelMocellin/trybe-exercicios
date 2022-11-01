d0c6a7ddeb25f53e674b7f4be40ab15644c3544b
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-11-01 15:50:34 -0300

    finalizada parte 1 do exercício

--- fundamentos/secao-05-javascript-dom/dia-03-eventos/script.js
+++ fundamentos/secao-05-javascript-dom/dia-03-eventos/script.js
@@ -13,5 +13,25 @@ const createDaysOfTheWeek = () => {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+const createNumberDays = () => {
+    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const daysList = document.querySelector('#days');
+    
+    for (let day of decemberDaysList) {
+        let dayItem = document.createElement('li');
+        dayItem.innerText = day;
+        dayItem.className = 'day';
+
+        if (day === 24 || day === 25 || day === 31) {
+            dayItem.classList.add('holiday');
+        }
+        if (day === 4 || day === 11 || day === 18 || day === 25) {
+            dayItem.classList.add('friday');
+        }
+
+        daysList.appendChild(dayItem);
+    }
+}
+
+createNumberDays();
   
