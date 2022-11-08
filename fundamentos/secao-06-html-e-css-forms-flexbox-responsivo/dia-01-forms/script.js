20c10840d8173d55eaf41f1932e8efa904a79329
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-11-08 15:10:57 -0300

    elaborando o exercício do dia

--- fundamentos/secao-06-html-e-css-forms-flexbox-responsivo/dia-01-forms/script.js
+++ fundamentos/secao-06-html-e-css-forms-flexbox-responsivo/dia-01-forms/script.js
@@ -1,18 +1,4 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
-
-HREF_LINK.addEventListener('click', (event) => {
+const submitBtn = document.querySelector('#submit-btn');
+submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
-})
-
-INPUT_CHECKBOX.addEventListener('click', (event) => {
-    event.preventDefault();
-})
-
-INPUT_TEXT.addEventListener('keypress', (event) => {
-    if (event.key !== 'a') {
-        event.preventDefault();
-    }
 })
