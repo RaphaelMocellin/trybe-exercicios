dcd2d346333ea7fb86b487223f735470109f32da
RaphaelMocellin <raphael.mocellin@gmail.com>
2022-11-23 11:31:22 -0300

    elaborado teste do exercicio 3

--- fundamentos/secao-07-es6-testes-unitarios/dia-03-macthers/printMessage.test.js
+++ fundamentos/secao-07-es6-testes-unitarios/dia-03-macthers/printMessage.test.js
@@ -4,4 +4,7 @@ describe('Testes da função print message', () => {
     test('verificando se há a propriedade personagem no obj', () => {
         expect(info).toHaveProperty('personagem');
     })
+    test('veriricando resposta da função', () => {
+        expect(printMessage(info)).toMatch('Boas vindas');
+    })
 })
